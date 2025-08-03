d17b01cb6b83a7849633a4ca7084b6270184d640
John-cyber-07 <lelukacomercial@gmail.com>
2025-08-03 20:07:28 -0300

    `Update dependencies and functions to use Firebase Functions v2 and callable functions`

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,19 @@
-import { onRequest } from "firebase-functions/https";
+import { onCall } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-export const assistenteHttp = onRequest((req, res) => {
-  logger.info("Recebi uma requisição:", req.body);
-  res.send("Olá da função assistenteHttp!");
+export const assistenteHttp = onCall((request) => {
+  // 'request.data' contém os dados enviados pelo cliente.
+  // 'request.auth' contém informações de autenticação do usuário (se logado).
+  logger.info("Função callable foi acionada!", {
+    data: request.data,
+    auth: request.auth,
+  });
+
+  // Em vez de res.send(), você simplesmente retorna um objeto.
+  // O SDK do Firebase cuida de enviá-lo de volta ao cliente.
+  return {
+    status: "success",
+    message: "Olá da função callable!",
+    receivedData: request.data
+  };
 });
